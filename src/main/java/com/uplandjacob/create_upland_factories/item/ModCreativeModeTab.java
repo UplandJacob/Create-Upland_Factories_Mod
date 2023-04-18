3e40ad725abd6b73cc8fa7f1a1f7cb00cd264c8b
Jacob Feil <jacob45feil@gmail.com>
2023-04-17 20:55:28 -0500

    basic

--- src/main/java/com/uplandjacob/create_upland_factories/item/ModCreativeModeTab.java
+++ src/main/java/com/uplandjacob/create_upland_factories/item/ModCreativeModeTab.java
@@ -4,7 +4,7 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
 public class ModCreativeModeTab {
-    public static final CreativeModeTab CREATE_JACOB_ADDITIONS = new CreativeModeTab("create_jacob_adds_tab") {
+    public static final CreativeModeTab CREATE_JACOB_ADDITIONS = new CreativeModeTab("create_fact_tab") {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(ModItems.INFUSED_REDSTONE.get());
